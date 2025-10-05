3ccadee717717d49c4a39eaeba38b524581b0a53
TAHER MALIK <76865073+tahermalik@users.noreply.github.com>
2025-10-05 16:55:51 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import dotenv from "dotenv";   // if using ES modules
-const app=express();
-
-dotenv.config();
-
-
-app.listen(process.env.PORT_NUM,()=>console.log(`${process.env.PORT_NUM}`))
-
