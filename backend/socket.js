0a3137093db83eb9a2af92102e42d91a19a4cd6e
Taher Malik <tahermalik2002@gmail.com>
2026-04-21 23:19:10 +0530

    More Origins added

--- backend/socket.js
+++ backend/socket.js
@@ -5,7 +5,7 @@ let io; // 🔥 shared variable
 export const initSocket = (server) => {
     io = new Server(server, {
         cors: {
-            origin: ["http://localhost:5173", "https://malikpetshop.onrender.com", "https://malik-pet-shop-git-main-taher-maliks-projects.vercel.app", "https://malik-pet-shop-iu40gnfxf-taher-maliks-projects.vercel.app"],  // your React frontend URL
+            origin: ["http://localhost:5173","http://localhost:5174", "https://malikpetshop.onrender.com", "https://malik-pet-shop-git-main-taher-maliks-projects.vercel.app", "https://malik-pet-shop-iu40gnfxf-taher-maliks-projects.vercel.app","https://malik-pet-shop-main.vercel.app","https://malik-pet-shop-main-irrlw7uzk-taher-maliks-projects.vercel.app"],  // your React frontend URL
 
             credentials: true
         }
